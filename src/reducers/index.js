c3843ef82b0db903e285cdeda9b84b364130b909
enres <enres@enress-MacBook-Pro.local>
2022-06-06 15:16:01 +0700

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+
+import postReducer from "./postReducer";
+import usersReducer from "./usersReducer";
+
+export default combineReducers({
+    posts: postReducer,
+    users: usersReducer
+});
+
+
